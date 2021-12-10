7c0fe1d834d02312dd2f40cf7d87370e2bcf0163
Laura Chan <chan@bryter.io>
2021-12-10 11:40:14 +0800

    wip

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,5 +1,60 @@
 use std::fs;
 
+struct Cave {
+    grid: Vec<Vec<usize>>,
+    low_points: Vec<(usize, usize)>,
+}
+
+impl Cave {
+    fn new(grid: Vec<Vec<usize>>) -> Cave {
+        let mut low_points: Vec<(usize, usize)> = vec![];
+
+        for (row_i, row) in grid.clone().iter().enumerate() {
+            for (col_i, sq) in row.into_iter().enumerate() {
+                let grid_slice = &grid;
+                let surrounding: [(i32, i32); 8] = [
+                    (-1, -1),
+                    (-1, -0),
+                    (-1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
+                ];
+                let surround_squares: [Option<&usize>; 8] = surrounding.map(|(x, y)| {
+                    let loc_row_index: i32 = row_i as i32 + x; // must ensure that everything is positive, as you cannot use negative usize
+                    let loc_col_index: i32 = col_i as i32 + y;
+                    if loc_col_index >= 0 && loc_row_index >= 0 {
+                        if let Some(r) = grid_slice.get(loc_row_index as usize) {
+                            if col_i as i32 + y >= 0 {
+                                if let Some(e) = r.get(loc_col_index as usize) {
+                                    return Some(e);
+                                };
+                            }
+                        };
+                    }
+                    None
+                });
+
+                let is_all_neighbor_squares_greater =
+                    surround_squares.iter().all(|neighbor_square| {
+                        if let Some(neighbor_square_num) = neighbor_square {
+                            &neighbor_square_num > &&sq
+                        } else {
+                            true
+                        }
+                    });
+                if is_all_neighbor_squares_greater {
+                    low_points.push((row_i, col_i))
+                }
+            }
+        }
+
+        Cave { grid, low_points }
+    }
+}
+
 fn main() {
     let file_name = "src/input.txt";
     let file_data = fs::read_to_string(file_name).unwrap();
@@ -10,52 +65,83 @@ fn main() {
         .map(|l| l.chars().map(|c| c.to_string().parse().unwrap()).collect())
         .collect();
 
-    let mut low_points: Vec<usize> = vec![];
+    let cave = Cave::new(grid);
 
-    // Your first goal is to find the low points - the locations that are lower than any of its adjacent locations.
-    for (row_i, row) in grid.clone().iter().enumerate() {
-        for (col_i, sq) in row.into_iter().enumerate() {
-            let grid_slice = &grid;
-            let surrounding: [(i32, i32); 8] = [
-                (-1, -1),
-                (-1, -0),
-                (-1, 1),
-                (0, -1),
-                (0, 1),
-                (1, -1),
-                (1, 0),
-                (1, 1),
-            ];
+    solve_1(&cave);
+    solve_2(&cave);
+}
 
-            let surround_squares: [Option<&usize>; 8] = surrounding.map(|(x, y)| {
-                let loc_row_index: i32 = row_i as i32 + x; // must ensure that everything is positive, as you cannot use negative usize
-                let loc_col_index: i32 = col_i as i32 + y;
-                if loc_col_index >= 0 && loc_row_index >= 0 {
-                    if let Some(r) = grid_slice.get(loc_row_index as usize) {
-                        if col_i as i32 + y >= 0 {
-                            if let Some(e) = r.get(loc_col_index as usize) {
-                                return Some(e);
-                            };
+fn solve_1(cave: &Cave) {
+    let low_points_sum = cave
+        .low_points
+        .iter()
+        .map(|(row_i, col_i)| cave.grid[*row_i][*col_i])
+        .sum::<usize>();
+    println!("Answer 1: {:?}", low_points_sum + cave.low_points.len());
+}
+
+fn solve_2(cave: &Cave) {
+    let mut pool_sizes: Vec<usize> = cave
+        .low_points
+        .iter()
+        .map(|low_point| find_pool_size(low_point, &cave.grid))
+        .collect::<Vec<usize>>();
+
+    pool_sizes.sort();
+
+    let largest_three_pool_sizes: Vec<usize> = pool_sizes[0..3].to_vec();
+    println!("{:?}", largest_three_pool_sizes);
+}
+
+fn find_pool_size(low_point: &(usize, usize), cave_grid: &Vec<Vec<usize>>) -> usize {
+    let (low_point_row, low_point_col) = low_point;
+    // todo!();
+    let mut is_done = false;
+    let mut pool_points: Vec<(usize, usize)> = vec![];
+    let mut current_row_i: usize = *low_point_row;
+    let mut current_column_i: usize = *low_point_col;
+    while !is_done {
+        let lower_starting_point = cave_grid[current_row_i][current_column_i];
+        let surrounding: [(i32, i32); 8] = [
+            (-1, -1),
+            (-1, -0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ];
+        let surround_squares: [Option<(usize, usize)>; 8] = surrounding.map(|(x, y)| {
+            let loc_row_index: i32 = current_row_i as i32 + x; // must ensure that everything is positive, as you cannot use negative usize
+            let loc_col_index: i32 = current_column_i as i32 + y;
+            if loc_col_index >= 0 && loc_row_index >= 0 {
+                if let Some(r) = cave_grid.get(loc_row_index as usize) {
+                    if current_column_i as i32 + y >= 0 {
+                        if let Some(e) = r.get(loc_col_index as usize) {
+                            if *e != 9 as usize && e - lower_starting_point == 1 {
+                                return Some((loc_row_index as usize, loc_col_index as usize));
+                            } else {
+                                return None;
+                            }
                         }
-                    };
+                    }
+                };
+            }
+            None
+        });
+
+        for sq in surround_squares {
+            if let Some(s) = sq {
+                let (s_x, s_y) = s;
+                if let None = pool_points.iter().position(|(x, y)| *x == s_x && *y == s_y) {
+                    pool_points.push(s)
                 }
-
-                None
-            });
-
-            let is_all_neighbor_squares_greater = surround_squares.iter().all(|neighbor_square| {
-                if let Some(neighbor_square_num) = neighbor_square {
-                    &neighbor_square_num > &&sq 
-                } else {
-                    true
-                }
-            });
-
-            if is_all_neighbor_squares_greater {
-                low_points.push(sq.clone())
             }
         }
+
+        is_done = true;
     }
 
-    println!("{:?}", low_points.iter().sum::<usize>() + low_points.len());
+    4
 }
